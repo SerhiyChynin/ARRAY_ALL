3fda88ddb8eec5f851e3b5ffa6de37ece5d3a423
SerhiiChynin <sergey.chinin@gmail.com>
2023-01-31 22:11:34 +0200

    join split hw done

---
+++ js/split_join.js
@@ -0,0 +1,36 @@
+//split - разбить строку и превратить ее в массив
+let str = 'Hello Baby today is a good day!';
+let s = str.split('', 19); // '' разделитель без пробела нужен для разделения слов, иначе вся строка целиком залетит в массив одним елементом
+console.log(str);         // через запятую можно указать колл возвращаемых элементов
+console.log(s);
+let m = str.split(' ')  //Если несколько слов ' ' разделитель с пробелом разделит слова между собой, иначе предложение будет вместе
+console.log(m);
+
+// join - позволяет обьединить массив в строку
+let zzz = ['h', 'cx', 'g', 't', 'r',];
+let xx2 = zzz.join('')
+console.log(xx2);
+console.log(zzz);
+
+// Home work
+
+let a11 = [13, 15, 22, 23, 26, 35, 72];
+
+b_1.onclick = () => {
+    let b = a11.join('');
+    out_1.innerHTML = b; 
+}
+
+
+b_2.onclick = () => {
+    let b = a11.join(' ');
+    out_2.innerHTML = b; 
+}
+
+
+b_3.onclick = () => {
+    let b = a11.join('=');
+    out_3.innerHTML = b; 
+}
+
+
