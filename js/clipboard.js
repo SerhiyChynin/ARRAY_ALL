994b90cd7b4bb193012b8cffcc640638f895037e
SerhiiChynin <sergey.chinin@gmail.com>
2023-02-04 13:28:44 +0200

    code

--- js/clipboard.js
+++ js/clipboard.js
@@ -1,5 +1,16 @@
 document.body.onclick = (event) => {
     const elem = event.target;
-    console.log(elem);
-
+    // console.log(elem);
+    if (elem.classList.contains('symbol')) {
+        console.log(elem);
+        navigator.clipboard.writeText(elem.innerHTML)
+            .then(() => {
+                document.querySelector('.out_1').innerHTML += 'copy<br>'
+            })
+            .catch(err => {
+            console.log(err);
+        })
+    }
 }
+// 😎 😅
+        
